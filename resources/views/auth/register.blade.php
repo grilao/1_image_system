d2d821e8e81015d8a97f46d52ff47bf5a59f1e2d
guigirelli <49819960+guigirelli@users.noreply.github.com>
2019-08-20 23:13:55 -0300

    login não pronto

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,44 +5,56 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+                <div class="card-header">{{ __('Novo usuário') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
+                    <form method="POST" action="{{ route('register') }}">
                         @csrf
 
-                        <input type="hidden" name="token" value="{{ $token }}">
-
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nome') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
-                                @error('email')
+                                @error('name')
                                     <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
+                                        <strong>Nome inválido</strong>
                                     </span>
                                 @enderror
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Endereço de e-mail') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>Email inválido</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Senha') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
+                                        <strong>Senha inválida</strong>
                                     </span>
                                 @enderror
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirmar senha') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
@@ -52,7 +64,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
+                                    {{ __('Cadastrar') }}
                                 </button>
                             </div>
                         </div>
--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -1,12 +1,16 @@
 <div class="menu-div">
     
     <div class="menu-div-center">
+
+        <hr>
         
         <div class="menu-div-div-img">
         
             <a href="{{ route('image.create') }}"><img class="menu-div-div-img-embrapa" src="{{ asset('images_sistema/Embrapablack.png') }}"></a>
         
         </div>
+
+        <hr>
         
     </div>
     <div class="menu-div-div-links">
@@ -25,10 +29,16 @@
                     <li><a style="width: 200px; margin-left: -40px;" class="link-nav" href="{{ route('image.index') }}">Lista de imagens</a></li>
                 </ul>
             </li>
-            <li style="margin-top: 200px;"><a style="width: 200px;" class="link-nav" href="">Ajuda</a></li>
-            <li><a style="width: 200px;" class="link-nav" href="{{ route('home') }}">Login</a></li>
+
+            <hr>
+
+            <li><a style="width: 200px;" class="link-nav" href="">Ajuda</a></li>
+            <li><a style="width: 200px;" class="link-nav" href="{{ route('home') }}">Entrar</a></li>
+            <li><a style="width: 200px;" class="link-nav" href="{{ route('home') }}">Novo usuário</a></li>
             <li><a style="width: 200px;" class="link-nav" href="https://www.embrapa.br/uva-e-vinho">Portal Embrapa</a></li>
             <li><a style="width: 200px;" class="link-nav" href="https://www.embrapa.br/login?p_p_id=58&p_p_lifecycle=0&_58_redirect=%2Fgroup%2Fintranet%2Fuva-e-vinho">Intranet Embrapa</a></li>
+            
+            <hr>            
 
         </ul>
         
